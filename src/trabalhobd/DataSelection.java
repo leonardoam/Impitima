939d338477c025e9c6b6725c3d4a9ccda2bb6381
leonardo <leonardo@DESKTOP-RDP754N>
2015-11-29 03:24:43 -0200

    new commit

--- src/trabalhobd/DataSelection.java
+++ src/trabalhobd/DataSelection.java
@@ -16,15 +16,39 @@ import java.sql.SQLException;
  */
 public class DataSelection {
     private Connection conn;
-    private PreparedStatement selectZonaStatement;
+    private PreparedStatement selectAllZonaStatement;
+    private PreparedStatement selectAllSecaoStatement;
+    private PreparedStatement selectAllUrnaStatement;
+    private PreparedStatement selectAllPartidoStatement;
+    private PreparedStatement selectAllPessoaStatement;
     
     public DataSelection(DBConnector conn) throws SQLException{
         this.conn = conn.getConnection();
         
-        selectZonaStatement = this.conn.prepareStatement("SELECT * FROM zona");
+        selectAllZonaStatement = this.conn.prepareStatement("SELECT * FROM zona WHERE 1=1 ORDER BY nroZona, estadoZona");
+        selectAllSecaoStatement = this.conn.prepareStatement("SELECT * FROM secao WHERE 1=1 ORDER BY nroZona, estadoZona, nroSecao");
+        selectAllUrnaStatement = this.conn.prepareStatement("SELECT * FROM urna WHERE 1=1 ORDER BY nroZona, estadoZona, nroSecao, nroUrna");
+        selectAllPartidoStatement = this.conn.prepareStatement("SELECT * FROM partido WHERE 1=1 ORDER BY nroPartido");
+        selectAllPessoaStatement = this.conn.prepareStatement("SELECT nroTitEleitor, nomePessoa, endPessoa, TO_CHAR(dataNasc, 'DD/MM/YYYY'), escolaridade, tipoPessoa, nroZona, estadoZona, nroSecao FROM pessoa WHERE 1=1 ORDER BY nomePessoa");
     }
     
     public ResultSet selectAllZonas() throws SQLException{
-        return selectZonaStatement.executeQuery();
+        return selectAllZonaStatement.executeQuery();
+    }
+    
+    public ResultSet selectAllSecao() throws SQLException{
+        return selectAllSecaoStatement.executeQuery();
+    }
+    
+    public ResultSet selectAllUrna() throws SQLException{
+        return selectAllUrnaStatement.executeQuery();
+    }
+    
+    public ResultSet selectAllPartido() throws SQLException{
+        return selectAllPartidoStatement.executeQuery();
+    }
+    
+    public ResultSet selectAllPessoa() throws SQLException{
+        return selectAllPessoaStatement.executeQuery();
     }
 }
