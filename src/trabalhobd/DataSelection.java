10f46343bd5b5658ffc98c1bfce017d88bb62af1
leonardo <leonardo@DESKTOP-RDP754N>
2015-12-01 04:24:02 -0200

    new commit

--- src/trabalhobd/DataSelection.java
+++ src/trabalhobd/DataSelection.java
@@ -23,6 +23,8 @@ public class DataSelection {
     private PreparedStatement selectAllPessoaStatement;
     private PreparedStatement selectAllFiliaStatement;
     private PreparedStatement selectAllFuncionarioStatement;
+    private PreparedStatement selectAllPessoaTitNomeStatement;
+    private PreparedStatement selectCandidatoPorCargoStatement;
     
     public DataSelection(DBConnector conn) throws SQLException{
         this.conn = conn.getConnection();
@@ -32,17 +34,21 @@ public class DataSelection {
         selectAllUrnaStatement = this.conn.prepareStatement("SELECT * FROM urna WHERE 1=1 ORDER BY nroZona, estadoZona, nroSecao, nroUrna");
         selectAllPartidoStatement = this.conn.prepareStatement("SELECT * FROM partido WHERE 1=1 ORDER BY nroPartido");
         selectAllPessoaStatement = this.conn.prepareStatement("SELECT nroTitEleitor, nomePessoa, endPessoa, TO_CHAR(dataNasc, 'DD/MM/YYYY'), escolaridade, tipoPessoa, nroZona, estadoZona, nroSecao FROM pessoa WHERE 1=1 ORDER BY nomePessoa");
+        selectAllPessoaTitNomeStatement = this.conn.prepareStatement("SELECT nrotiteleitor, nomepessoa FROM PESSOA");
         selectAllFiliaStatement = this.conn.prepareStatement("SELECT pessoa.nrotiteleitor, pessoa.nomepessoa, partido.nropartido, partido.siglapartido, partido.nomepartido"
                                                             + " FROM FILIA, PESSOA, PARTIDO "
                                                             + " WHERE filia.nrotiteleitor = pessoa.nrotiteleitor"
                                                             + "  AND filia.nropartido = partido.nropartido"
                                                             + "  ORDER BY partido.nroPartido ASC");
-        
         selectAllFuncionarioStatement = this.conn.prepareStatement("SELECT pessoa.nrotiteleitor, pessoa.nomepessoa, pessoa.endpessoa, TO_CHAR(dataNasc, 'DD/MM/YYYY'), pessoa.escolaridade, pessoa.tipopessoa, pessoa.nrozona, pessoa.estadozona, pessoa.nrosecao, funcionario.cargofunc"
                                                             + " FROM PESSOA"
                                                             + " LEFT JOIN FUNCIONARIO"
                                                             + " ON PESSOA.NROTITELEITOR = FUNCIONARIO.NROTITELEITOR"
                                                             + " ORDER BY pessoa.nomepessoa");
+        selectCandidatoPorCargoStatement = this.conn.prepareStatement("SELECT candidato.nrotiteleitor, pessoa.nomepessoa, candidato.nomefantasia, candidato.cargocandidato\n" +
+                                                                        "  FROM PESSOA, CANDIDATO\n" +
+                                                                        "  WHERE PESSOA.NROTITELEITOR = CANDIDATO.NROTITELEITOR\n" +
+                                                                        "    AND UPPER(CANDIDATO.CARGOCANDIDATO) LIKE UPPER(?)");
     }
     
     public ResultSet selectAllZonas() throws SQLException{
@@ -64,6 +70,9 @@ public class DataSelection {
     public ResultSet selectAllPessoa() throws SQLException{
         return selectAllPessoaStatement.executeQuery();
     }
+    public ResultSet selectAllTitNomePessoa() throws SQLException{
+        return selectAllPessoaTitNomeStatement.executeQuery();
+    }
     
     public ResultSet selectAllFilia() throws SQLException{
         return selectAllFiliaStatement.executeQuery();
@@ -71,5 +80,10 @@ public class DataSelection {
     
     public ResultSet selectAllFuncionario() throws SQLException{
         return selectAllFuncionarioStatement.executeQuery();
+    }
+    
+    public ResultSet selectCandidatoPorCargo(String cargo) throws SQLException{
+        selectCandidatoPorCargoStatement.setString(1, cargo);
+        return selectCandidatoPorCargoStatement.executeQuery();
     }
 }
